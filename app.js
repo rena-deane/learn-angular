296600fda957581aeefc706025b04993ce74b8b1
Rena Goldsmith <rena.kg@gmail.com>
2016-09-12 00:47:13 +1200

    add quantity to products objects

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 
   app.controller('StoreController', function() {
     this.products = icecreams
+
+    this.checkQty = function (quantity) {
+      return quantity === 0
+    }
   })
 
   var icecreams = [
     {
       name: 'Chocolate Monolit',
       price: 2.5,
+      quantity: 10,
       description: 'Made with real chocolate. Not cocoa powder!',
       image: 'http://cdn6.bigcommerce.com/s-owv5nj/product_images/uploaded_images/chocolate-ice-cream.jpg?t=1463032837',
       reviews: [
@@ -22,6 +27,7 @@
     {
       name: 'Vanilla',
       price: 2,
+      quantity: 6,
       description: 'Made with real vanilla beans. Not vanilla essence!',
       image: 'http://www.proprofs.com/quiz-school/upload/yuiupload/1231897629.jpg',
       reviews: [
@@ -35,6 +41,7 @@
     {
       name: 'Hockey Pockey',
       price: 4,
+      quantity: 0,
       description: 'Made with real hockey pockey. Not frozen golden syrup!',
       image: 'https://www.fonterrafoodservice.co.nz/thumbs/600x600r/2015-09/tt-0000s-0012-hokeypokey.png',
       reviews: [
@@ -53,6 +60,7 @@
     {
       name: 'Sundae Mondaye Fundaye Pundaye',
       price: 7,
+      quantity: 2,
       description: 'Made with everything - not recommended if you have allergies',
       image: 'http://cdn1.bostonmagazine.com/wp-content/uploads/2013/06/7-5.jpg',
       reviews: [
